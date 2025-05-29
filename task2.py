96e0b891db01d5a9e32e8640e06740c6e1ece1d8
Kavish Vijan <kavishvijan461@gmail.com>
2025-05-30 02:29:04 +0530

    Add files via upload
    Task 2: Using the Math Module for Calculations

---
+++ task2.py
@@ -0,0 +1,13 @@
+#Task 2: Using the Math Module for Calculations
+
+#modules
+
+a= float(input('Enter a number:'))
+
+from math import *
+
+print('square root:',sqrt(a))
+
+print('logarithm:',log(a))
+
+print("sine:",sin(a))
